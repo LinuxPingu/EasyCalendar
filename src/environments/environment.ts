494077c8a8728f20f1aae252d657ac03daf0b5ca
dhernandez0599@gmail.com <47802332+LinuxPingu@users.noreply.github.com>
2023-03-29 14:52:45 -0600

    Setup with Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'fbtester-e3b82',
+    appId: '1:265776828820:web:53de69e6528027b5b23f81',
+    databaseURL: 'https://fbtester-e3b82-default-rtdb.firebaseio.com',
+    storageBucket: 'fbtester-e3b82.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBziy5gq3CcxvyT2XyMhUol0mcYwagxSRA',
+    authDomain: 'fbtester-e3b82.firebaseapp.com',
+    messagingSenderId: '265776828820',
+    measurementId: 'G-K09S3L3ST8',
+  },};
