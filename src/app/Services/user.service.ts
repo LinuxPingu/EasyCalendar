46e7c3b4684dea78169c4e8aed13d360b161aaf8
dhernandez0599@gmail.com <47802332+LinuxPingu@users.noreply.github.com>
2023-04-02 12:25:43 -0600

    Login and register with Google

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
-import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
-import {User} from '../Models/user.model';
+import { Auth, createUserWithEmailAndPassword, GoogleAuthProvider, user} from '@angular/fire/auth';
+import { User } from '../Models/user.model';
 import { API_URL } from '../Helpers/app_constants';
 import { HttpClient } from '@angular/common/http';
+import { fetchSignInMethodsForEmail, signInWithPopup, UserCredential } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -38,6 +40,52 @@ export class UserService{
             let asError = error as Error;
             console.error(error);
             return asError;
+        }
+    }
+
+    async register_by_google(result:UserCredential){
+            /*Build User*/
+            let new_user: User={
+                id_User: result.user.uid,
+                email: result.user.email ?? 'no_mail',
+                username: result.user.displayName ?? 'no_username',
+                password: 'by_Google',
+                is_Admin: false,
+                is_Active: true
+            }
+
+            this.post_user_to_API(new_user);
+    }
+
+    async login_with_google():Promise<boolean | Error>{
+        try {
+            let provider = new GoogleAuthProvider();
+            let result = await signInWithPopup(this.fbAuth,provider);
+            let signInMethods =  await fetchSignInMethodsForEmail(this.fbAuth,result.user.email ?? "none");
+
+            if(signInMethods.includes('google.com')){
+              
+                if(await this.get_user_by_UID(result.user.uid) === null){
+                    /*Post user to API*/
+                    await this.register_by_google(result);
+                }
+            }
+
+            return true;
+        } catch (error) {
+            let asError = error as Error;
+            console.error(error);
+            return asError;
+        }
+    }
+
+    async get_user_by_UID(uid:string){
+        try {
+            let found_user = await this.http.get(API_URL + this.user_enpoint+`/users/${uid}`).toPromise();
+            return found_user;
+        } catch (error) {
+            console.error(error);
+            return null;
         }
     }
 }
--- src/app/Views/login/login.component.ts
+++ src/app/Views/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { UserService } from 'src/app/Services/user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router:Router){
+  constructor(private router:Router, private service: UserService){
 
+  }
+
+  async sign_up_google(){
+    let result = await this.service.login_with_google();
+    console.log(result);
   }
 }
